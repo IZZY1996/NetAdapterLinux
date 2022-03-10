4ba813c5fec88b51227b908855ad2a82b9dffe1f
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-03-09 20:55:34 -0600

    Update and rename NetAdapter.ps1 to NetAdapter.psm1

--- NetAdapter.psm1
+++ NetAdapter.psm1
@@ -21,6 +21,13 @@ function Get-NetAdapter {
       for ($i = 0; $i -lt $adapts.length; $i = $i + 2) {
         $adapter = New-Object psobject
         $ifindex,$adaptname,$d = $adapts[$i].tostring().split(": ")
+        $status, $d = $d.split(" ")
+        if ($status -eq "<>") {
+            $status = "Disconnected"
+        }
+        else {
+                $status = "Up"
+        }
         $mac = $adapts[$i+1].tostring()
         $mac = $mac.split(" ")
         $mediatype = $mac[$mac.length-2]
@@ -37,6 +44,7 @@ function Get-NetAdapter {
         }
         Add-Member -InputObject $adapter -MemberType NoteProperty -name Name -Value $adaptname
         Add-Member -InputObject $adapter -MemberType NoteProperty -name ifIndex -Value $ifindex
+        Add-Member -InputObject $adapter -MemberType NoteProperty -name Status -Value $status
         Add-Member -InputObject $adapter -MemberType NoteProperty -name MacAddress -Value $mac
         Add-Member -InputObject $adapter -MemberType NoteProperty -name MediaType -Value $mediatype
         $adapters += $adapter
