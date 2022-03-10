1dbfd010e8a7cee803cf0c11d5a7bdcd0da97210
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-03-09 19:54:17 -0600

    Update NetAdapter.ps1

--- NetAdapter.ps1
+++ NetAdapter.ps1
@@ -15,8 +15,24 @@ function Get-NetAdapter {
     )
     
     process {
-      $adapts = (ip address)
-      Write-Host "$adapts"
+      $adapts = (ip link)
+      $adapters = @()
+      
+      for ($i = 0; $i -lt $adapts.length; $i = $i + 2) {
+        $adapter = New-Object psobject
+        $ifindex,$adaptname,$d = $adapts[$i].tostring().split(": ")
+        $mac = $adapts[$i+1].tostring()
+        $mac = $mac.split(" ")
+        $mac = $mac[$mac.length-1]
+        $mac = $mac.replace(":","-")
+        Add-Member -InputObject $adapter -MemberType NoteProperty -name Name -Value $adaptname
+        Add-Member -InputObject $adapter -MemberType NoteProperty -name ifIndex -Value $ifindex
+        Add-Member -InputObject $adapter -MemberType NoteProperty -name MacAddress -Value $mac
+        $adapters += $adapter
+      }
+      
+      $adapters
+      
     }
 }
 
